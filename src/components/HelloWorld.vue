0718e78bb6c94fb13f4933199a2286d80a0679bd
carmel26 <nkeshipelagie@gmail.com>
2024-01-21 22:47:14 +0300

    v-show, v-text, v-html at 21Jan2024

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,12 @@
 <template>
   <div class="hello">
     <button @click="handleClick"> Click me! </button>
-    <h1 id="header">Hi, my name is : {{ name.toUpperCase() }}</h1>
-    <input v-model="text" type="text" name="mytext" id="mytext"> <br>
-    {{ text }}
+    <h1 id="header">Hi, my name is : {{ myName.toUpperCase() }}</h1>
+    <h3 v-once>{{ myName }}</h3>
+    <button @click="myName = 'Programmer And Developer'"> Change name </button>
+    <div v-html="myContent"></div>
+    <div v-show="isShowing" v-text="myText"></div>
+    <button @click="isShowing = !isShowing"> {{ isShowing ? 'Hide' : 'Show' }} </button>
   </div>
 </template>
 
@@ -21,9 +24,11 @@ export default {
   },
   data() {
     return {
-      name: 'Carmel Nkeshimana',
+      myName: 'Carmel Nkeshimana',
       number: 4,
-      text: '',
+      myContent: '<b style="color:green"> This is my content </b>',
+      myText: 'Hello word, im learning Vue JS!',
+      isShowing: true,
     };
   },
   props: {
